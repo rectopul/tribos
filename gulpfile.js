d752d442f440cc70bf09c7adab7c1d284106c630
Lucas Costa <lucas.garcia@auaha.com.br>
2019-11-28 11:41:29 -0300

    base ok

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ const   { watch, src, dest, parallel, series }      = require('gulp'),
         cleanCSS                                    = require('gulp-clean-css');
 
 const gulp = require('gulp');  
-        
+const shell = require('gulp-shell')
+
 function css() {
     return src('src/*.styl')
         .pipe( stylus({
@@ -19,6 +20,7 @@ function css() {
         }) )
         .pipe(rename( "app.min.css"))
         .pipe(dest('opencode/css'))
+        .pipe(shell(['cd opencode && opencode upload css/app.min.css']))
 }
 
 function js() {
@@ -36,12 +38,14 @@ function js() {
         .pipe(dest('opencode/js', { sourcemaps: true }))
 }
 
+ 
+
 exports.js          = js;
 exports.css         = css;
 
 exports.init = series(css, js);
 exports.default = function() {
-    watch('src/**/*.styl', series( css ));
+    watch('src/**/*.styl', series( css));
     watch('src/**/*.js', series( js ));
 };
 
--- opencode/js/app.js
+++ opencode/js/app.js
@@ -1,25 +1,4 @@
 
 
-console.log('button')
-
-
-console.log('button')
-
-
-console.log('button')
-
-
-console.log('button')
-
-
-console.log('button')
-
-
-
-console.log('button')
-
-
-console.log('button3')
-
 ;
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJpbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJcblxuQGltcG9ydCAnY29tcG9uZW50cy9idXR0b24vaW5kZXguanMnOyJdLCJmaWxlIjoiYXBwLmpzIn0=
