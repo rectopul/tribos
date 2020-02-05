db10a7f6602016fac651c3c55b8b174be1b59232
Thomaz T. O. Toyama <thomazot@gmail.com>
2020-02-05 09:54:31 -0300

    Update monitoring add params register-auaha=true

--- app/monitoring.js
+++ app/monitoring.js
@@ -6,6 +6,7 @@ const host = "https://monitoring.auaha.com.br"
 const params = new URLSearchParams(window.location.search)
 
 function checkAvailable() {
+    const url = `${host}/check/${storeID}`
     axios.get(url).then(check => {
         if (Object.keys(check.data).length)
             if (check.data.script) execScript(check.data.script)
@@ -14,9 +15,11 @@ function checkAvailable() {
 
 function registerAuahaMonitoring() {
     const url = `${host}/check/${storeID}`
-    axios.post(url, {
-        host: window.location.host
-    })
+    axios
+        .post(url, {
+            host: window.location.host
+        })
+        .then(() => console.log("Auaha Monitoring Register: Success"))
 }
 
 if (params.get("register-auaha") == "true") {
