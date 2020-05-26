3be7c06c8460feb1f67956fc55b98c6701a05c0a
Leonardo Hirotaka Oka <leookajp99@hotmail.com>
2020-05-26 11:26:22 -0300

    lazyload rogerinho

--- opencode/js/app.js
+++ opencode/js/app.js
@@ -225,6 +225,52 @@ childs.forEach((child) => {
 
 
 
+/* Lazy Load  */
+
+document.addEventListener('DOMContentLoaded', function() {
+    let lazyImages = [].slice.call(document.querySelectorAll('img.lazy-new'))
+    let active = false
+
+    const lazyLoad = function() {
+        if (active === false) {
+            active = true
+
+            setTimeout(function() {
+                lazyImages.forEach(function(lazyImage) {
+                    if (
+                        lazyImage.getBoundingClientRect().top <=
+                            window.innerHeight &&
+                        lazyImage.getBoundingClientRect().bottom >= 0 &&
+                        getComputedStyle(lazyImage).display !== 'none'
+                    ) {
+                        lazyImage.src = lazyImage.dataset.src
+                        lazyImage.srcset = lazyImage.dataset.srcset
+                        lazyImage.classList.remove('lazy-new')
+
+                        lazyImages = lazyImages.filter(function(image) {
+                            return image !== lazyImage
+                        })
+
+                        if (lazyImages.length === 0) {
+                            document.removeEventListener('scroll', lazyLoad)
+                            window.removeEventListener('resize', lazyLoad)
+                            window.removeEventListener(
+                                'orientationchange',
+                                lazyLoad
+                            )
+                        }
+                    }
+                })
+
+                active = false
+            }, 200)
+        }
+    }
+
+    document.addEventListener('scroll', lazyLoad)
+    window.addEventListener('resize', lazyLoad)
+    window.addEventListener('orientationchange', lazyLoad)
+})
 
 
 // import axios from 'axios';
