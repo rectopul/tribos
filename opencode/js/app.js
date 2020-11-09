b49e2adc1765b6e7586ad31bf95a78b2b0dc7759
Rogério Bonfim <rogeriomateus@icloud.com>
2020-11-09 01:42:52 -0300

    click variations

--- opencode/js/app.js
+++ opencode/js/app.js
@@ -58,225 +58,233 @@ jQuery('.button__close--navigation').click(function() {
 
 const tool = (() => {
     //private var/functions
-    const prices = {};
+    const prices = {}
+
+    function selectVariation(selector) {
+        const selectors = [...document.querySelectorAll(selector)]
+
+        if (!selectors) return
+
+        selectors.forEach((variation) => {
+            variation.addEventListener('click', function (e) {
+                e.preventDefault()
+
+                const container = variation.closest('.toolCategory__column.variations')
+
+                if (container.classList.contains('disabled')) return
+
+                const input = variation.querySelector('input')
+
+                const product = variation.closest('.toolCategory').querySelector('.product__header.slick-center')
+
+                if (input && product) {
+                    const image = product.querySelector('figure')
+
+                    if (image) image.innerHTML = `<img src="${input.value}">`
+                }
+            })
+        })
+    }
 
     function getPrice() {
-        const pricesArray = Object.keys(prices);
+        const pricesArray = Object.keys(prices)
 
         const pricesValue = pricesArray.map((item) => {
-            if (prices[item].disabled == false) return prices[item].value;
-            else return 0;
-        });
+            if (prices[item].disabled == false) return prices[item].value
+            else return 0
+        })
 
-        let total = 0;
+        let total = 0
 
-        pricesValue.forEach((item) => (total = total + item));
+        pricesValue.forEach((item) => (total = total + item))
 
-        console.log(`Total`, total);
+        const currencyPrice = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+        }).format(total)
 
-        const currencyPrice = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-        }).format(total);
+        const totalElement = document.querySelector('.toolCategory__column--summary--total .price__total')
 
-        console.log(`prices: `, pricesValue);
+        if (totalElement) totalElement.innerHTML = currencyPrice
 
-        const totalElement = document.querySelector(
-            ".toolCategory__column--summary--total .price__total"
-        );
-
-        if (totalElement) totalElement.innerHTML = currencyPrice;
-
-        return currencyPrice;
+        return currencyPrice
     }
 
     function changePrice(element) {
-        const productPrice = element.dataset.price;
+        const productPrice = element.dataset.price
 
-        const category = element.closest(".toolCategory").dataset.category;
+        const category = element.closest('.toolCategory').dataset.category
 
-        const variant = element.dataset.variant;
+        const variant = element.dataset.variant
 
-        const product = element.dataset.productId;
+        const product = element.dataset.productId
 
         if (prices[category]) {
-            prices[category].product = product;
-            prices[category].value = parseFloat(productPrice);
+            prices[category].product = product
+            prices[category].value = parseFloat(productPrice)
         } else {
             prices[category] = {
                 product,
                 value: parseFloat(productPrice),
                 disabled: false,
-            };
+            }
         }
 
-        if (variant) prices[category]["variant"] = variant;
+        if (variant) prices[category]['variant'] = variant
 
-        return getPrice();
+        return getPrice()
     }
 
     function togglePrice(element) {
-        const category = element.closest(".toolCategory").dataset.category;
+        const category = element.closest('.toolCategory').dataset.category
         if (element.checked) {
-            if (
-                prices[category] &&
-                prices[category].hasOwnProperty("disabled")
-            ) {
-                prices[category].disabled = true;
+            if (prices[category] && prices[category].hasOwnProperty('disabled')) {
+                prices[category].disabled = true
             } else {
                 prices[category] = {
                     disabled: true,
                     value: 0,
-                };
+                }
             }
         } else {
-            if (
-                prices[category] &&
-                prices[category].hasOwnProperty("disabled")
-            ) {
-                prices[category].disabled = false;
+            if (prices[category] && prices[category].hasOwnProperty('disabled')) {
+                prices[category].disabled = false
             } else {
                 prices[category] = {
                     disabled: false,
                     value: 0,
-                };
+                }
             }
         }
 
-        return getPrice();
+        return getPrice()
     }
 
     function getCategories() {
-        if (!document.body.dataset.toolCategories) return null;
+        const productsActive = [...document.querySelectorAll('.product__header.slick-center')]
 
-        const categories = document.body.dataset.toolCategories.split(",");
+        console.log(`Lista Produto: `, productsActive)
 
-        return categories;
+        if (!productsActive) return
+
+        productsActive.forEach((element) => {
+            console.log(`Produto: `, element)
+            return changePrice(element)
+        })
+
+        return true
     }
 
     function init() {
-        if (document.body.classList.contains("tool"))
-            console.log(getCategories());
+        if (document.body.classList.contains('tool')) return getCategories()
     }
 
     function variationsCarousel(selector) {
-        const list = [...document.querySelectorAll(selector)];
+        const list = [...document.querySelectorAll(selector)]
 
-        if (!list) return;
-
-        (($) => {
+        if (!list) return
+        ;(($) => {
             list.map((item) => {
                 $(item).slick({
                     infinite: false,
                     speed: 150,
                     draggable: false,
                     fade: true,
-                    cssEase: "linear",
+                    cssEase: 'linear',
                     slidesToShow: 1,
                     slidesToScroll: 1,
                     initialSlide: 1,
                     nextArrow: false,
                     prevArrow: false,
-                });
-            });
-        })(jQuery);
+                })
+            })
+        })(jQuery)
     }
 
     function removeProduct(selector) {
-        const inputs = [...document.querySelectorAll(selector)];
+        const inputs = [...document.querySelectorAll(selector)]
 
-        if (!inputs) return;
+        if (!inputs) return
 
         inputs.map((input) => {
-            input.addEventListener("change", function (e) {
-                e.preventDefault();
+            input.addEventListener('change', function (e) {
+                e.preventDefault()
 
-                const span = input.closest("header").querySelector("span");
-                const container = input.closest(
-                    ".toolCategory__column--summary"
-                );
+                const span = input.closest('header').querySelector('span')
+                const container = input.closest('.toolCategory__column--summary')
 
                 if (input.checked) {
-                    span.innerHTML = `Removido`;
-                    container.classList.add("removed");
-                    togglePrice(input);
+                    span.innerHTML = `Removido`
+                    container.classList.add('removed')
+                    togglePrice(input)
                 } else {
-                    span.innerHTML = `Remover`;
-                    container.classList.remove("removed");
-                    togglePrice(input);
+                    span.innerHTML = `Remover`
+                    container.classList.remove('removed')
+                    togglePrice(input)
                 }
-            });
-        });
+            })
+        })
     }
 
     function handleChangePrice(element) {
-        const theprice = element.dataset.price;
+        const theprice = element.dataset.price
 
-        const name = element.dataset.name;
+        const name = element.dataset.name
 
-        changePrice(element);
+        changePrice(element)
 
-        const image = element.querySelector("figure img").cloneNode();
+        const image = element.querySelector('figure img').cloneNode()
 
-        const summary = element
-            .closest(".toolCategory")
-            .querySelector(".toolCategory__column--summary");
+        const summary = element.closest('.toolCategory').querySelector('.toolCategory__column--summary')
 
-        if (!summary) return;
+        if (!summary) return
 
-        image.setAttribute("width", "70px");
-        image.style.maxWidth = "70px";
+        image.setAttribute('width', '70px')
+        image.style.maxWidth = '70px'
 
-        summary.querySelector("figure").innerHTML = "";
-        summary.querySelector("figure").append(image);
-        summary.querySelector("h3").innerHTML = name;
-        summary.querySelector("h2").innerHTML = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-        }).format(theprice);
+        summary.querySelector('figure').innerHTML = ''
+        summary.querySelector('figure').append(image)
+        summary.querySelector('h3').innerHTML = name
+        summary.querySelector('h2').innerHTML = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+        }).format(theprice)
     }
 
     function handleSlickProductChange(event, slick, currentSlide, nextSlide) {
         //get active slick product id $slides[2].attributes
-        const activeSlick = slick.$slides[currentSlide].dataset.id;
+        const activeSlick = slick.$slides[currentSlide].dataset.id
 
-        if (!activeSlick) return;
+        if (!activeSlick) return
 
         const variationsContainer = slick.$slides[currentSlide]
-            .closest(".toolCategory")
-            .querySelector(".toolCategory__column.variations");
+            .closest('.toolCategory')
+            .querySelector('.toolCategory__column.variations')
 
-        handleChangePrice(slick.$slides[currentSlide]);
+        handleChangePrice(slick.$slides[currentSlide])
 
-        const variationsFromProduct = document.querySelector(
-            `.tool__variants--wrapper[data-product="${activeSlick}"]`
-        );
+        const variationsFromProduct = document.querySelector(`.tool__variants--wrapper[data-product="${activeSlick}"]`)
 
         if (!variationsFromProduct) {
-            if (variationsContainer)
-                variationsContainer.classList.add("disabled");
+            if (variationsContainer) variationsContainer.classList.add('disabled')
 
-            return;
+            return
         } else {
-            if (variationsContainer)
-                variationsContainer.classList.remove("disabled");
+            if (variationsContainer) variationsContainer.classList.remove('disabled')
         }
 
-        const indexVariation = variationsFromProduct.dataset.slickIndex;
+        const indexVariation = variationsFromProduct.dataset.slickIndex
 
-        const slickVariation = variationsFromProduct.closest(
-            ".toolCategory__column--variations"
-        );
+        const slickVariation = variationsFromProduct.closest('.toolCategory__column--variations')
 
-        $(slickVariation).slick.slickGoTo(indexVariation);
+        $(slickVariation).slick.slickGoTo(indexVariation)
     }
 
     function productCarousel(selector) {
-        const list = [...document.querySelectorAll(selector)];
+        const list = [...document.querySelectorAll(selector)]
 
-        if (!list) return;
-
-        (($) => {
+        if (!list) return
+        ;(($) => {
             list.map((item) => {
                 $(item).slick({
                     infinite: false,
@@ -284,18 +292,18 @@ const tool = (() => {
                     slidesToShow: 3,
                     slidesToScroll: 1,
                     centerMode: true,
-                    centerPadding: "0px",
+                    centerPadding: '0px',
                     initialSlide: 1,
-                    nextArrow:
-                        '<button class="slick-next"><span class="icon-arrow"></span></button>',
-                    prevArrow:
-                        '<button class="slick-prev"><span class="icon-arrow"></span></button>',
-                });
+                    nextArrow: '<button class="slick-next"><span class="icon-arrow"></span></button>',
+                    prevArrow: '<button class="slick-prev"><span class="icon-arrow"></span></button>',
+                })
 
                 // On before slide change
-                $(item).on("afterChange", handleSlickProductChange);
-            });
-        })(jQuery);
+                $(item).on('afterChange', handleSlickProductChange)
+            })
+
+            init()
+        })(jQuery)
     }
 
     return {
@@ -304,17 +312,19 @@ const tool = (() => {
         productCarousel,
         variationsCarousel,
         removeProduct,
-    };
-})();
+        selectVariation,
+    }
+})()
 
-tool.init();
+tool.removeProduct(`.toolCategory__column--summary header input`)
 
-tool.removeProduct(`.toolCategory__column--summary header input`);
-
-tool.productCarousel(".toolCategory__column.products .showcase");
+tool.productCarousel('.toolCategory__column.products .showcase')
 
 //toolCategory__column--variations
-tool.variationsCarousel(".toolCategory__column--variations");
+tool.variationsCarousel('.toolCategory__column--variations')
+
+//tool__variants--list
+tool.selectVariation(`.tool__variants--list > li`)
 
 //
 
@@ -390,6 +400,8 @@ if(button) {
 }
 
 
+ 
+
 
 // function FakeSelect() {
 //     $('select').each(function(){
@@ -417,8 +429,6 @@ if(button) {
 // FakeSelect();
 
 // document.addEventListener('FAKESELECT', () => { FakeSelect() }, false);
-
- 
 
 
 
