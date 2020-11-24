0b90378bf7cbbe166e1826a60ce3cfa007a93cf8
admin <admin@iMac-de-admin.local>
2020-11-24 14:29:15 -0300

    correcao 1

--- opencode/js/app.js
+++ opencode/js/app.js
@@ -906,48 +906,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-const tableClose = document.querySelector('.tabela-medidas__close')
-
-//Close tabela de medidas
-
-if (tableClose) {
-    tableClose.addEventListener('click', function (e) {
-        e.preventDefault()
-
-        const modal = tableClose.closest('.tabela-medidas')
-
-        if (modal) modal.classList.add('hide')
-    })
-}
-
-//Open tabela
-
-jQuery(document).on('thumbs:start', function () {
-    const OpenTable = document.querySelector('.tabela-medidas__title')
-
-    const sizeVariation = document.querySelector('.cor_variacao.passo2')
-
-    if (sizeVariation && OpenTable) {
-        if (!sizeVariation.querySelector('.tabela-medidas__title')) {
-            const openClone = OpenTable.cloneNode(true)
-
-            openClone.addEventListener('click', function (e) {
-                e.preventDefault()
-
-                const modal = document.querySelector('.tabela-medidas')
-
-                if (modal) modal.classList.remove('hide')
-            })
-
-            sizeVariation.append(openClone)
-
-            console.log(`Table title: `, sizeVariation)
-        }
-
-        //OpenTable.remove()
-    }
-})
-
 var $ = $tray
 if (typeof $ == 'function') {
     $(document).ready(function () {
@@ -1018,6 +976,48 @@ if (typeof $ == 'function') {
         }
     }
 }
+
+const tableClose = document.querySelector('.tabela-medidas__close')
+
+//Close tabela de medidas
+
+if (tableClose) {
+    tableClose.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const modal = tableClose.closest('.tabela-medidas')
+
+        if (modal) modal.classList.add('hide')
+    })
+}
+
+//Open tabela
+
+jQuery(document).on('thumbs:start', function () {
+    const OpenTable = document.querySelector('.tabela-medidas__title')
+
+    const sizeVariation = document.querySelector('.cor_variacao.passo2')
+
+    if (sizeVariation && OpenTable) {
+        if (!sizeVariation.querySelector('.tabela-medidas__title')) {
+            const openClone = OpenTable.cloneNode(true)
+
+            openClone.addEventListener('click', function (e) {
+                e.preventDefault()
+
+                const modal = document.querySelector('.tabela-medidas')
+
+                if (modal) modal.classList.remove('hide')
+            })
+
+            sizeVariation.append(openClone)
+
+            console.log(`Table title: `, sizeVariation)
+        }
+
+        //OpenTable.remove()
+    }
+})
 
 
  
