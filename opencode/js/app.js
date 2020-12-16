619c477d379406c7e0896f857c0ce572e62c91df
Bruno Lopes <bruno.lopes@auaha.com.br>
2020-12-16 01:28:10 -0300

    ajuste sabes

--- opencode/js/app.js
+++ opencode/js/app.js
@@ -636,9 +636,9 @@ $('.banner--grid .banner__list').not('.slick-initialized').slick({
         jQuery(this).not('.slick-initialized').slick({
             mobileFirst: false,
             slidesToShow: 8,
-            slidesToScroll: 8,
-            prevArrow: `<button aria-label="prev" type="button" class="slick-prev"><svg width="45px" height="45px" viewBox="0 0 45 45" ><circle id="Oval" stroke="#E8E8E8" stroke-width="2" fill="#FFFFFF" cx="22.5" cy="22.5" r="21.5"></circle><g id="seta" transform="translate(24.000000, 22.500000) rotate(-180.000000) translate(-24.000000, -22.500000) translate(19.000000, 15.000000)" fill="#FF7922" fill-rule="nonzero"><polygon id="Path" transform="translate(5.000000, 7.500000) rotate(-270.000000) translate(-5.000000, -7.500000) " points="10.7312305 2.5 5 8.68341699 -0.731230469 2.5 -2.5 4.4082599 5 12.5 12.5 4.4082599"></polygon></g></svg></button>`,
-            nextArrow: `<button aria-label="prev" type="button" class="slick-next"><svg width="45px" height="45px" viewBox="0 0 45 45" ><circle id="Oval" stroke="#E8E8E8" stroke-width="2" fill="#FFFFFF" cx="22.5" cy="22.5" r="21.5"></circle><g id="seta" transform="translate(24.000000, 22.500000) rotate(-180.000000) translate(-24.000000, -22.500000) translate(19.000000, 15.000000)" fill="#FF7922" fill-rule="nonzero"><polygon id="Path" transform="translate(5.000000, 7.500000) rotate(-270.000000) translate(-5.000000, -7.500000) " points="10.7312305 2.5 5 8.68341699 -0.731230469 2.5 -2.5 4.4082599 5 12.5 12.5 4.4082599"></polygon></g></svg></button>`,
+            slidesToScroll: 1,
+            prevArrow: `<button aria-label="prev" type="button" class="slick-prev"><svg id="icon-seta-carrosel" viewBox="0 0 39 32"><path d="M9.914 0s-0.302 4.378 0.152 4.83c0.281 0.327 0.635 0.584 1.037 0.748l0.018 0.007v-0.452c4.679 1.66 9.057 3.773 13.578 5.584 1.102 0.514 2.454 1.027 3.845 1.449l0.23 0.060c-14.633-1.508-28.773-1.508-28.773-1.508 8.027 4.638 17.523 7.678 27.654 8.439l0.221 0.013c-3.471 2.264-6.943 4.528-10.566 6.642-1.055 0.604 0.604 5.887 0.15 6.188 5.234-2.916 9.573-5.664 13.754-8.621l-0.471 0.316c2.109-1.509 7.547-4.226 7.999-6.793 0.152-1.055 0.755-5.584-0.15-6.34-3.623-2.869-9.811-4.226-14.189-5.736-4.123-1.923-8.96-3.582-13.991-4.731l-0.498-0.096zM34.668 14.641c-0.604 0.452-1.359 0.906-1.963 1.358 0.005-0.126 0.008-0.273 0.008-0.421 0-0.651-0.058-1.288-0.17-1.907l0.010 0.065c0.755 0.302 1.511 0.604 2.115 0.906z"></path></svg></button>`,
+            nextArrow: `<button aria-label="prev" type="button" class="slick-next"><svg id="icon-seta-carrosel" viewBox="0 0 39 32"><path d="M9.914 0s-0.302 4.378 0.152 4.83c0.281 0.327 0.635 0.584 1.037 0.748l0.018 0.007v-0.452c4.679 1.66 9.057 3.773 13.578 5.584 1.102 0.514 2.454 1.027 3.845 1.449l0.23 0.060c-14.633-1.508-28.773-1.508-28.773-1.508 8.027 4.638 17.523 7.678 27.654 8.439l0.221 0.013c-3.471 2.264-6.943 4.528-10.566 6.642-1.055 0.604 0.604 5.887 0.15 6.188 5.234-2.916 9.573-5.664 13.754-8.621l-0.471 0.316c2.109-1.509 7.547-4.226 7.999-6.793 0.152-1.055 0.755-5.584-0.15-6.34-3.623-2.869-9.811-4.226-14.189-5.736-4.123-1.923-8.96-3.582-13.991-4.731l-0.498-0.096zM34.668 14.641c-0.604 0.452-1.359 0.906-1.963 1.358 0.005-0.126 0.008-0.273 0.008-0.421 0-0.651-0.058-1.288-0.17-1.907l0.010 0.065c0.755 0.302 1.511 0.604 2.115 0.906z"></path></svg></button>`,
             responsive: [
                 {
                     breakpoint: 425,
@@ -744,6 +744,7 @@ if(button) {
 
 
 
+
 function debounce(func){
     var timer;
     return function(event){
@@ -806,7 +807,6 @@ childs.forEach((child) => {
 
 
 
-
 /* Lazy Load  */
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -855,6 +855,7 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 
+
 // import axios from 'axios';
 
 // document.addEventListener("DOMContentLoaded", function(){
@@ -898,7 +899,6 @@ document.addEventListener('DOMContentLoaded', function() {
 //         }
 //     }
 // });
-
 
 
 
